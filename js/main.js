f7c711e8cadc6be5f388c6c56882e6e99b9d8af2
Megan Coningsby <meganconingsby@gmail.com>
2023-10-21 17:42:08 +1100

    Add checkGameOver function

--- js/main.js
+++ js/main.js
@@ -159,3 +159,15 @@ function checkWin() {
         }
     }
 }
+
+// Checks if it is 'game over'
+function checkGameOver() {
+    for (let i = 0; i < board.length; i++) {
+        for (let j = 0; j < board[i].length; j++) {
+            if (board[i][j] === 0) {
+                return false;
+            }
+        }
+    }
+    return true;
+}
